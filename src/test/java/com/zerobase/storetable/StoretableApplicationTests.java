8fcf1cfc964f3b84bedf6bffbd605b8e91cb4d7f
Ha Su-bin <8205subin@gmail.com>
2023-06-14 11:30:11 +0900

    first commit

---
+++ src/test/java/com/zerobase/storetable/StoretableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.storetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoretableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

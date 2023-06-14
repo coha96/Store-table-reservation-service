d669a7b6a0f283ea75ef1d9e436140d456c55941
Ha Su-bin <8205subin@gmail.com>
2023-06-14 11:33:32 +0900

    IDE commit test

--- src/main/java/com/zerobase/storetable/StoretableApplication.java
+++ src/main/java/com/zerobase/storetable/StoretableApplication.java
@@ -8,6 +8,7 @@ public class StoretableApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoretableApplication.class, args);
+        // IDE 커밋 테스트용
     }
 
 }
